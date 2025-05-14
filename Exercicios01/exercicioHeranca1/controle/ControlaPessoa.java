b115995f6779ca34b2ae922565d574e238d97d36
AlexandreLouzada <49560198+AlexandreLouzada@users.noreply.github.com>
2025-05-14 10:19:53 -0300

    Atualização exercicios

---
+++ Exercicios01/exercicioHeranca1/controle/ControlaPessoa.java
@@ -0,0 +1,17 @@
+package Exercicios01.exercicioHeranca1.controle;
+
+import Exercicios01.exercicioHeranca1.dominio.*;
+
+public class ControlaPessoa {
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno("João", 20, "Engenharia");
+        Professor professor = new Professor("Maria", 45, 7500.50);
+
+        System.out.println("Dados do Aluno:");
+        aluno.exibirDados();
+
+        System.out.println("\nDados do Professor:");
+        professor.exibirDados();
+    }
+}
+
