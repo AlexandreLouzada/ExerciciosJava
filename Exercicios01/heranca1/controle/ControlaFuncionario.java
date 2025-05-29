c0ecad88d0c86cb57dc94cb82db584b75d9435b5
AlexandreLouzada <49560198+AlexandreLouzada@users.noreply.github.com>
2025-05-29 19:28:27 -0300

    Atualizar exercicios

---
+++ Exercicios01/heranca1/controle/ControlaFuncionario.java
@@ -0,0 +1,15 @@
+package Exercicios01.heranca1.controle;
+
+import Exercicios01.heranca1.dominio.*;
+
+public class ControlaFuncionario {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario("Mariana",5000);
+        Gerente gerente = new Gerente("Anderson", 10000);
+        Estagiario estagiario = new Estagiario ("Ana",4000);
+
+        System.out.println("Nome:"+funcionario.getNome()+" Salario final:"+funcionario.calcularSalarioBase());
+        System.out.println("Nome:"+estagiario.getNome()+" Salario final:"+estagiario.calcularSalarioBase());
+        System.out.println("Nome:"+gerente.getNome()+" Salario final:"+gerente.calcularSalarioBase());
+    }
+}
