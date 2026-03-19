e4860c890b30721656081380496f757dec6aac16
AlexandreLouzada <49560198+AlexandreLouzada@users.noreply.github.com>
2026-03-19 14:58:15 -0300

    Atualizações da lista
    Atualizações da lista

--- fase05/futebol/controle/ControlaClube.java
+++ fase05/futebol/controle/ControlaClube.java
@@ -1,32 +1,40 @@
 package fase05.futebol.controle;
 
 import java.util.Scanner;
-
 import fase05.futebol.dominio.Clube;
 
 public class ControlaClube {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Digite o nome: ");
         String nome = sc.nextLine();
-        System.out.print("Digite a quantidade de vitorias: ");
+
+        System.out.print("Digite a quantidade de vitórias: ");
         int vitorias = sc.nextInt();
+
         System.out.print("Digite a quantidade de empates: ");
         int empates = sc.nextInt();
+
         System.out.print("Digite a quantidade de derrotas: ");
         int derrotas = sc.nextInt();
 
+        // Validação simples
+        if (vitorias < 0 || empates < 0 || derrotas < 0) {
+            System.out.println("Valores inválidos! Não podem ser negativos.");
+            sc.close();
+            return;
+        }
+
         Clube clube = new Clube(nome, vitorias, empates, derrotas);
+
         System.out.println("\nDados do clube:");
         System.out.println("Nome: " + clube.getNome());
         System.out.println("Total de jogos: " + clube.totalDeJogos());
-        System.out.println("Pontos ganhos:  " + clube.pontosGanhos());
-        System.out.println("Pontos perdidos " + clube.pontosPerdidos() );
-        System.out.println("Aproveitamento " + clube.aproveitamento() );
+        System.out.println("Pontos ganhos: " + clube.pontosGanhos());
+        System.out.println("Pontos perdidos: " + clube.pontosPerdidos());
+        System.out.printf("Aproveitamento: %.2f%%\n", clube.aproveitamento());
 
-        sc.close();  // Fechar o Scanner após o uso
-
-  }
-
+        sc.close();
+    }
 }
--- fase05/futebol/dominio/Clube.java
+++ fase05/futebol/dominio/Clube.java
@@ -1,64 +1,67 @@
 package fase05.futebol.dominio;
 
 public class Clube {
-   private String nome;
-   private int vitorias;
-   private int empates;
-   private int derrotas;
-  
-public Clube(String nome, int vitorias, int empates, int derrotas) {
-    this.nome = nome;
-    this.vitorias = vitorias;
-    this.empates = empates;
-    this.derrotas = derrotas;
+    private String nome;
+    private int vitorias;
+    private int empates;
+    private int derrotas;
+
+    public Clube(String nome, int vitorias, int empates, int derrotas) {
+        this.nome = nome;
+        this.vitorias = vitorias;
+        this.empates = empates;
+        this.derrotas = derrotas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public int totalDeJogos() {
+        return vitorias + empates + derrotas;
+    }
+
+    public int pontosGanhos() {
+        return vitorias * 3 + empates;
+    }
+
+    public int pontosPerdidos() {
+        return totalDeJogos() * 3 - pontosGanhos();
+    }
+
+    public double aproveitamento() {
+        if (totalDeJogos() == 0) {
+            return 0.0;
+        }
+        return (double) pontosGanhos() / (totalDeJogos() * 3) * 100;
+    }
+
 }
-
-public String getNome() {
-    return nome;
-}
-
-public void setNome(String nome) {
-    this.nome = nome;
-}
-
-public int getVitorias() {
-    return vitorias;
-}
-
-public void setVitorias(int vitorias) {
-    this.vitorias = vitorias;
-}
-
-public int getEmpates() {
-    return empates;
-}
-
-public void setEmpates(int empates) {
-    this.empates = empates;
-}
-
-public int getDerrotas() {
-    return derrotas;
-}
-
-public void setDerrotas(int derrotas) {
-    this.derrotas = derrotas;
-}
-
-public int totalDeJogos(){
-   return  (vitorias + derrotas + empates);
-}
-
-public int pontosGanhos(){
-   return  vitorias * 3 + empates;
-}
-
-public int pontosPerdidos(){
-   return  derrotas * 3 + empates * 2;
-}   
-
-public double aproveitamento(){
-   return  (pontosGanhos()/(totalDeJogos()*3)) * 100;
-}
-
-  }
